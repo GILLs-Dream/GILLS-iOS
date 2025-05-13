ecc17498940db545a61bcabc049467e6732e4b25
luckyyy <oyslucy@naver.com>
2025-05-14 01:20:03 +0900

    [Design/#2] add UIFont extension

---
+++ GILLsDREAM-iOS/Global/Extensions/UIFont+.swift
@@ -0,0 +1,72 @@
+//
+//  UIFont+.swift
+//  GILLsDREAM-iOS
+//
+//  Created by 오연서 on 5/14/25.
+//
+
+import UIKit
+
+extension UIFont {
+    enum Pretendard {
+        case thin
+        case extraLight
+        case light
+        case regular
+        case medium
+        case semibold
+        case bold
+        case extrabold
+        case black
+        
+        var name: String {
+            switch self {
+            case .thin: return "Pretendard-Thin"
+            case .extraLight: return "Pretendard-ExtraLight"
+            case .light: return "Pretendard-Light"
+            case .regular: return "Pretendard-Regular"
+            case .medium: return "Pretendard-Medium"
+            case .semibold: return "Pretendard-SemiBold"
+            case .bold: return "Pretendard-Bold"
+            case .extrabold: return "Pretendard-ExtraBold"
+            case .black: return "Pretendard-Black"
+            }
+        }
+    }
+    
+    static func pretendard(_ weight: Pretendard, size: CGFloat) -> UIFont {
+        return UIFont(name: weight.name, size: size) ?? UIFont.systemFont(ofSize: size)
+    }
+}
+
+extension UIFont {
+    enum PretendardStyle {
+        case title1
+        case title2
+        case title3
+        case title4
+        case subtitle1
+        case subtitle2
+        case subtitle3
+        case subtitle4
+        case body1
+        case body2
+        case body3
+        
+        var font: UIFont {
+            switch self {
+            case .title1: return UIFont.pretendard(.semibold, size: 30)
+            case .title2: return UIFont.pretendard(.medium, size: 30)
+            case .title3: return UIFont.pretendard(.regular, size: 30)
+            case .title4: return UIFont.pretendard(.light, size: 30)
+            case .subtitle1: return UIFont.pretendard(.semibold, size: 24)
+            case .subtitle2: return UIFont.pretendard(.medium, size: 24)
+            case .subtitle3: return UIFont.pretendard(.regular, size: 24)
+            case .subtitle4: return UIFont.pretendard(.light, size: 24)
+            case .body1: return UIFont.pretendard(.medium, size: 15)
+            case .body2: return UIFont.pretendard(.medium, size: 12)
+            case .body3: return UIFont.pretendard(.medium, size: 12)
+            }
+        }
+    }
+}
